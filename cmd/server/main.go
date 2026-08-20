c36663c6e830619314864d71261535313be5a123
alexnakagama <alexnakagama4@gmail.com>
2026-08-20 20:40:55 -0300

    added more comments

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,12 +8,15 @@ import (
 func main() {
 
 	// When a request to / execute this function
+	// Request contins what the client sent
+	// ResponseWriter is the object used to respond to the client
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Loca-Go API")
 	})
 
 	fmt.Println("server running on port: 8080")
 
+	// This function tells to open a server in x port and start to accept requests
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
