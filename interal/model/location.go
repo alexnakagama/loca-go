db58e9b706f5ee1c516fb75a8fa5894d0c8ee89f
alexnakagama <alexnakagama4@gmail.com>
2026-08-21 01:28:37 -0300

    added json tags to location model

--- interal/model/location.go
+++ interal/model/location.go
@@ -1,7 +1,7 @@
 package model
 
 type Location struct {
-	PersonID int
-	Latitude float64
-	Longitude float64
+	PersonID  int     `json:"person_id"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
 }
