857c59b0f39b2da6e2a37c5b7b7d77f5ec170d59
alexnakagama <alexnakagama4@gmail.com>
2026-08-21 01:18:42 -0300

    starting again

--- interal/model/location.go
+++ interal/model/location.go
@@ -1,13 +1,7 @@
 package model
 
-import (
-	"time"
-)
-
 type Location struct {
-	ID        int       `json:"id"`
-	PersonID  int       `json:"person_id"`
-	Latitude  float64   `json:"latitude"`
-	Longitude float64   `json:"longitude"`
-	CreatedAt time.Time `json:"created_at"`
+	PersonID int
+	Latitude float64
+	Longitude float64
 }
--- interal/model/person.go
+++ interal/model/person.go
@@ -1,6 +1,6 @@
 package model
 
 type Person struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
+	ID        int       
+	Name      string    
 }
