83846e5bcca889bde0f78727eda98bfaef0dbb2d
alexnakagama <alexnakagama4@gmail.com>
2026-08-22 01:40:22 -0300

    deleted that now its not useful

--- internal/handler/person_handler.go
+++ internal/handler/person_handler.go
@@ -1,19 +1,4 @@
 package handler
 
 import (
-	"net/http"
-
-	"github.com/alexnakagama/loca-go/internal/store"
 )
-
-type PersonHandler struct {
-	store *store.Store
-}
-
-func NewPersonHandler(store *store.Store) *PersonHandler {
-	return &PersonHandler{
-		store: store,
-	}
-} 
-
-func (h *PersonHandler) HandlePerson(w http.ResponseWriter, r *http.Request) {}
