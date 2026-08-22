32f703fc31d628bf22b7a7f4b264a9377f78c251
alexnakagama <alexnakagama4@gmail.com>
2026-08-22 00:20:50 -0300

    finished function for validate

--- internal/model/location.go
+++ internal/model/location.go
@@ -1,7 +1,25 @@
 package model
 
+import "errors"
+
 type Location struct {
 	PersonID  int     `json:"person_id"`
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
+}
+
+func (l Location) Validate() error {
+	if l.PersonID <= 0 {
+		return errors.New("ERROR: invalid PersonID")
+	}
+
+	if l.Latitude < -90 || l.Latitude > 90 {
+		return errors.New("ERROR: invalid Latitude")
+	}
+
+	if l.Longitude < -180 || l.Longitude > 180 {
+		return errors.New("ERROR: invalid Longitude")
+	}
+
+	return nil
 }
