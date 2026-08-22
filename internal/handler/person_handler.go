30c0e76411150c6e3067a9817cdd67679daaef87
alexnakagama <alexnakagama4@gmail.com>
2026-08-22 01:50:08 -0300

    finished function for the person handler

--- internal/handler/person_handler.go
+++ internal/handler/person_handler.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/alexnakagama/loca-go/internal/model"
 	"github.com/alexnakagama/loca-go/internal/store"
 )
 
@@ -16,4 +18,25 @@ func NewPersonHandler(store *store.Store) *PersonHandler {
 	}
 }
 
-func (h *PersonHandler) HandlePerson(w http.ResponseWriter, r * http.Request) {}
+func (h *PersonHandler) HandlePerson(w http.ResponseWriter, r * http.Request) {
+	var person model.Person
+
+	err := json.NewDecoder(r.Body).Decode(&person)
+	if err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+	
+	err = person.Validate()
+	if err != nil {
+		http.Error(w, "invalid person", http.StatusBadRequest)
+		return
+	}
+
+	h.store.SetPerson(person)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(person)
+}
