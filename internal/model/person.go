101cd8f4a87f9c2927c5ca98e6d9930de6ad5335
alexnakagama <alexnakagama4@gmail.com>
2026-08-22 00:35:46 -0300

    finished validate function for a person

--- internal/model/person.go
+++ internal/model/person.go
@@ -1,6 +1,16 @@
 package model
 
+import "errors"
+
 type Person struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+func (p Person) Validate() error {
+	if p.ID <= 0 {
+		return errors.New("ERROR: invalid ID")
+	}
+
+	return nil
 }
